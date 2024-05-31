3cafd34d853b68194f0232e7b539928da96f43e3
Marwan Mohammed <116826792+Rookie-kinda@users.noreply.github.com>
2024-05-31 16:09:12 +0300

    Update bulkSender.py

--- bulkSender.py
+++ bulkSender.py
@@ -1,48 +1,39 @@
 # links[i] = f"https://wa.me/+20{numbers[i]}"
 
 import pywhatkit as pwk # to send whatsapp msgs
-from datetime import datetime # to determine current time
 from time import sleep # to pause between moves since internet is...shitty at best
 import pyautogui as pag # to control keyboard
 import pandas as pd # ...pandas
 import keyboard as kb
 
+# To handle incoming data that contains whatsapp numbers and owner's name
+def read_data(path, type_of_file):
+    if type_of_file == "excel":
+        data = pd.read_excel(path)
+    elif type_of_file == "csv":
+        data = pd.read_csv(path)
+    else:
+        raise ValueError(f"'type_of_file' should be either 'excel' or 'csv', not {type(type_of_file)}")
+    self.data.dropna(inplace=True, subset="phone_num) #To drop rows with no phone number
 
-class dataHandler:
-    def __init__(self, path_to_data, type_of_file):
-        self.path = path_to_data
-        
-        if type_of_file == "excel":
-            self.data = pd.read_excel(self.path)
-        elif type_of_file == "csv":
-            self.data = pd.read_csv(self.path)
-        else:
-            raise ValueError(f"'type_of_file' should be either 'excel' or 'csv', not {type(type_of_file)}")
-        self.data.dropna(inplace=True)
-        self.data.set_index("Parent's WA", inplace=True)
 
-    def check_siblings(self) -> None:
-        counts = self.data.value_counts(subset="Parent's WA")
-        counts = counts[counts > 1]
-        
-        for i in counts.index:
-            x = self.data.loc[i].drop_duplicates()["Name"]
-            self.data["Name"].loc[i] = " و".join(x)
-        
-        self.data.drop_duplicates(inplace=True)
-        return self.data
-        # self.data.to_excel("E:\مسجد العادل(دورة القران الكريم)\dummy.xlsx")
+def sendmsg(phone: str, msg: str) -> None:
+    """
+    Adjust wait_time properly, if the internet connection is slow then increase it (although 30 seconds should be good anyways)
+    And if the connection is good then decrease it (10-15 seconds should be good, unless you have some exceptionally good connection, lol, but either way 10 to 15 seccs should be good)
+    Anways, adjust how you see fit!
 
-class whatsend:
-    def __init__(self, wait_time) -> None:
-        self.wait_time = wait_time
-    def sendmsg(self, phone: str, msg: str) -> None:
+    Also, change tab_close if you want to
+
+    One last thing is, adjust the country code where to the proper country code, this code is Egypt's code and the condition is to check if the number starts with a 0 or not, because Egypt's is +20
+    but we always start our phones with "01" anways, so it's just to avoid an error, that's all, you can change it with a try except block though...idk why but that's an option
+    """
         if phone[0] == "0":
-            pwk.sendwhatmsg_instantly(f'+2{phone}', msg, wait_time=self.wait_time, tab_close=True)
+            pwk.sendwhatmsg_instantly(f'+2{phone}', msg, wait_time=30, tab_close=True)
         else:
-            pwk.sendwhatmsg_instantly(f'+20{phone}', msg, wait_time=self.wait_time, tab_close=True)
-    # sleep(15)
-    # kb.press_and_release("enter")
-    # sleep(3)
+            pwk.sendwhatmsg_instantly(f'+20{phone}', msg, wait_time=30, tab_close=True)
+    kb.press_and_release("enter") # because sometimes the function doesn't actually send the message, just types it in the box
+    # The next block is in case your internet is slow and you want to close the tab, should not be necessary if you adjust wait_time properly though, so you can delete it
+    # sleep(5)
     # with pag.hold("ctrl"):
     #     pag.press("w")
